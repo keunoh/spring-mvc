a5d96755902499e2d4f8b16aab8e05d565748896
keunoh <rmsdhcjswo1@gmail.com>
2023-10-31 23:37:34 +0900

    FrontController - ModelView

--- README.md
+++ README.md
@@ -418,10 +418,29 @@ MVC 패턴을 적용한 덕분에 컨트롤러의 역할과 뷰를 렌더링하�
 *V2 구조*  
 ![FRONT_CONTROLLER_V2](https://github.com/keunoh/spring-mvc/assets/96904103/ae809172-b0f9-4f0d-8a08-65f6b1823a4d)
 
+### Model 추가 - v3
+*서블릿 종속성 제거*  
+컨트롤러 입장에서 HttpServletRequest, HttpServletResponse가 꼭 필요할까?
+요청 파라미터 정보는 자바의 Map으로 대신 넘기도록 하면 지금 구조에서는 컨트롤러가 서블릿 기술을 몰라도 동작할 수 있다.
+그리고 request 객체를 Model로 사용하는 대신에 별도의 Model 객체를 만들어서 반환하면 된다.
+우리가 구현하는 컨트롤러가 서블릿 기술을 전혀 사용하지 않도록 변경해 보자.
+이렇게 하면 구현코드도 매우 단순해지고, 테스트 코드 작성도 쉽다.
 
+*뷰 이름 중복 제거*  
+컨트롤러에서 지정하는 뷰 이름에 중복이 있는 것을 확인할 수 있다.
+컨트롤러는 `뷰의 논리 이름`을 반환하고, 실제 물리 위치의 이름은 프론트 컨트롤러에서 처리하도록 단순화하자.
+이렇게 해두면 향후 뷰의 폴더 위치가 함께 이동해도 프론트 컨트롤러만 고치면 된다.
 
+![FRONT_CONTROLLER_V3](https://github.com/keunoh/spring-mvc/assets/96904103/31e7c77e-ddac-4a18-8f95-651311da5d6c)
 
+*ModelView*  
+지금까지 컨트롤러에서 서블릿에 종속적인 HttpServletRequest를 사용했다.
+그리고 Model도 request.setAttribute()를 통해 데이터를 저장하고 뷰에 전달했다.
+서블릿의 종속성을 제거하기 위해 Model을 직접 만들고, 추가로 View 이름까지 전달하는 객체를 만들어보자.
+(이번 버전에서는 컨트롤러에서 HttpServletRequest를 사용할 수 없다. 따라서 직접 request.setAttribute()를 호출할 수도 없다. Model이 별도로 필요하다.)
 
+*뷰 리졸버*  
+컨트롤러가 반환한 논리 뷰 이름을 실제 물리 뷰 경로로 변경한다. 그리고 실제 물리 경로가 있는 MyView객체를 반환한다.
 
 
 
---
+++ src/main/java/hello/servlet/web/frontcontroller/v3/FrontControllerV3.java
@@ -0,0 +1,72 @@
+package hello.servlet.web.frontcontroller.v3;
+
+import hello.servlet.web.frontcontroller.ModelView;
+import hello.servlet.web.frontcontroller.MyView;
+import hello.servlet.web.frontcontroller.v2.ControllerV2;
+import hello.servlet.web.frontcontroller.v2.controller.MemberFormControllerV2;
+import hello.servlet.web.frontcontroller.v2.controller.MemberListControllerV2;
+import hello.servlet.web.frontcontroller.v2.controller.MemberSaveControllerV2;
+import hello.servlet.web.frontcontroller.v3.controller.MemberFormControllerV3;
+import hello.servlet.web.frontcontroller.v3.controller.MemberListControllerV3;
+import hello.servlet.web.frontcontroller.v3.controller.MemberSaveControllerV3;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.util.HashMap;
+import java.util.Map;
+
+@WebServlet(name = "frontControllerV3", urlPatterns = "/front-controller/v3/*")
+public class FrontControllerV3 extends HttpServlet {
+
+    private Map<String, ControllerV3> controllerMap = new HashMap<>();
+
+    public FrontControllerV3() {
+        controllerMap.put("/front-controller/v3/members/new-form", new MemberFormControllerV3());
+        controllerMap.put("/front-controller/v3/members/save", new MemberSaveControllerV3());
+        controllerMap.put("/front-controller/v3/members", new MemberListControllerV3());
+
+    }
+
+    @Override
+    protected void service(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        String requestURI = request.getRequestURI();
+        ControllerV3 controller = controllerMap.get(requestURI);
+
+        if (controller == null) {
+            response.setStatus(HttpServletResponse.SC_NOT_FOUND);
+            return;
+        }
+
+        // 브라우저에서 받은 값을 이용해 파라미터값을 생성해준다.
+        Map<String, String> paramMap = createParamMap(request);
+
+        // 파라미터를 각 컨트롤러에 보내주고 이용하도록 하게 한다.
+        // 그리고 처리된 값을 모델뷰를 생성해서 셋팅하고 반환해준다.
+        ModelView mv = controller.process(paramMap);
+
+        // 생성된 모델뷰의 논리이름을 가져온다.
+        String viewName = mv.getViewName();
+
+        // 뷰 리졸버를 이용해서 마이뷰를 생성하고 리턴한다.
+        MyView view = viewResolver(viewName);
+
+        // 모델뷰에 담긴 모델 값과 request response 가지고 렌더링한다.
+        view.render(mv.getModel(), request, response);
+    }
+
+    private static MyView viewResolver(String viewName) {
+        return new MyView("/WEB-INF/views/" + viewName + ".jsp");
+    }
+
+    private static Map<String, String> createParamMap(HttpServletRequest request) {
+        Map<String, String> paramMap = new HashMap<>();
+        request.getParameterNames().asIterator()
+                .forEachRemaining(paramName -> paramMap.put(paramName, request.getParameter(paramName)));
+
+        return paramMap;
+    }
+}
