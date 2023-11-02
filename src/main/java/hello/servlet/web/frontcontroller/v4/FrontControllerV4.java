ac38bc82ff37baa5c436bf2194feff5e908aba83
keunoh <rmsdhcjswo1@gmail.com>
2023-11-02 20:45:16 +0900

    MVC Framework

---
+++ src/main/java/hello/servlet/web/frontcontroller/v4/FrontControllerV4.java
@@ -0,0 +1,65 @@
+package hello.servlet.web.frontcontroller.v4;
+
+import hello.servlet.web.frontcontroller.MyView;
+import hello.servlet.web.frontcontroller.v4.controller.MemberFormControllerV4;
+import hello.servlet.web.frontcontroller.v4.controller.MemberListControllerV4;
+import hello.servlet.web.frontcontroller.v4.controller.MemberSaveControllerV4;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.util.HashMap;
+import java.util.Map;
+
+@WebServlet(name = "frontControllerV4", urlPatterns = "/front-controller/v4/*")
+public class FrontControllerV4 extends HttpServlet {
+
+    private Map<String, ControllerV4> controllerMap = new HashMap<>();
+
+    public FrontControllerV4() {
+        controllerMap.put("/front-controller/v4/members/new-form", new MemberFormControllerV4());
+        controllerMap.put("/front-controller/v4/members/save", new MemberSaveControllerV4());
+        controllerMap.put("/front-controller/v4/members", new MemberListControllerV4());
+
+    }
+
+    @Override
+    protected void service(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        String requestURI = request.getRequestURI();
+        ControllerV4 controller = controllerMap.get(requestURI);
+
+        if (controller == null) {
+            response.setStatus(HttpServletResponse.SC_NOT_FOUND);
+            return;
+        }
+
+        // 브라우저에서 받은 값을 이용해 파라미터값을 생성해준다.
+        Map<String, String> paramMap = createParamMap(request);
+        Map<String, Object> model = new HashMap<>();
+
+        // 파라미터를 각 컨트롤러에 보내주고 이용하도록 하게 한다.
+        // 그리고 처리된 값을 모델뷰를 생성해서 셋팅하고 반환해준다.
+        String viewName = controller.process(paramMap, model);
+
+        // 뷰 리졸버를 이용해서 마이뷰를 생성하고 리턴한다.
+        MyView view = viewResolver(viewName);
+
+        // 모델뷰에 담긴 모델 값과 request response 가지고 렌더링한다.
+        view.render(model, request, response);
+    }
+
+    private static MyView viewResolver(String viewName) {
+        return new MyView("/WEB-INF/views/" + viewName + ".jsp");
+    }
+
+    private static Map<String, String> createParamMap(HttpServletRequest request) {
+        Map<String, String> paramMap = new HashMap<>();
+        request.getParameterNames().asIterator()
+                .forEachRemaining(paramName -> paramMap.put(paramName, request.getParameter(paramName)));
+
+        return paramMap;
+    }
+}
